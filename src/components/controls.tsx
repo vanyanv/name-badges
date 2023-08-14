fb7cad856fe86710bb290ffb5e6135329e75d9b8
Vardan Vanyan <vardan.vanyan@gmail.com>
2023-08-14 14:42:23 -0700

    added typescript props

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types';
 
-const ControlPanel = ({ name, onChange }) => {
+type ControlPanelProps = {
+  name: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+};
+
+const ControlPanel = ({ name, onChange }: ControlPanelProps) => {
   return (
     <form
-      className="flex flex-row gap-4 text-white bg-primary-10"
+      className="bg-primary-10 flex flex-row gap-4 text-white"
       onSubmit={(event) => event.preventDefault()}
     >
       <div>
